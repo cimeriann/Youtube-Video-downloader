7aa326dfb44c8bb8493c88d59990d375ab74eff3
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-02-18 13:49:41 +0100

    updated

--- ytdownload.py
+++ ytdownload.py
@@ -3,7 +3,14 @@ from pytube.cli import on_progress
 
 link = input('> Enter link: ')
 print()
-yt = YouTube(link, on_progress_callback=on_progress)
+yt = YouTube( link, on_progress_callback=on_progress)
 
 print(f'Video title: {yt.title}')
 print(f'video has {yt.views} views')
+print()
+
+save_path = "C:\\Users\\Hp Folio\\Desktop"
+video = yt.streams.get_highest_resolution()
+video.download(output_path=save_path)
+
+print('Video successfully downloaded')
